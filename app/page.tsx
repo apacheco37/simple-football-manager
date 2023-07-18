323b7fe0481515aae9fbcbd7fbb40f68bbc4a436
Andres Pacheco <apacheco73@hotmail.com>
2023-07-18 11:58:14 -0300

    Basic leagues implementation

--- app/page.tsx
+++ app/page.tsx
@@ -2,20 +2,38 @@
 
 import { useState } from "react";
 import { useLiveQuery } from "dexie-react-hooks";
+import {
+  Button,
+  Input,
+  ListItem,
+  Stack,
+  UnorderedList,
+  Text,
+} from "@chakra-ui/react";
+
 import styles from "./page.module.css";
-import { db } from "./db";
-import { Button, Input, ListItem, UnorderedList } from "@chakra-ui/react";
+import { db } from "../db/db";
 
 export default function Home() {
   const [name, setName] = useState("");
-  const friends = useLiveQuery(() => db.teams.toArray());
+  const [invalidInput, setInvalidInput] = useState(false);
+  const leagues = useLiveQuery(() => db.leagues.toArray());
 
-  const addPlayer = async () => {
+  const createNewLeague = async () => {
     try {
-      if (!name) return;
+      if (!name) {
+        setInvalidInput(true);
+        return;
+      }
 
-      await db.teams.add({
+      await db.leagues.add({
         name,
+        teams: [
+          { name: "Team 1" },
+          { name: "Team 2" },
+          { name: "Team 3" },
+          { name: "Team 4" },
+        ],
       });
 
       setName("");
@@ -24,19 +42,36 @@ export default function Home() {
     }
   };
 
+  const onInputChange = (inputString: string) => {
+    if (inputString && invalidInput) {
+      setInvalidInput(false);
+    }
+    setName(inputString);
+  };
+
   return (
     <main className={styles.main}>
-      <Button onClick={addPlayer}>Add player</Button>
-      <Input
-        type="text"
-        value={name}
-        onChange={(ev) => setName(ev.target.value)}
-      />
-      <UnorderedList>
-        {friends?.map((friend) => (
-          <ListItem key={friend.id}>{friend.name}</ListItem>
-        ))}
-      </UnorderedList>
+      <Stack spacing={12}>
+        <Stack direction={"row"}>
+          <Input
+            isInvalid={invalidInput}
+            className={styles.input}
+            type="text"
+            value={name}
+            placeholder="League Name"
+            onChange={(e) => onInputChange(e.target.value)}
+          />
+          <Button className={styles.button} onClick={createNewLeague}>
+            Create New League
+          </Button>
+        </Stack>
+        <Text>Or load an existing one:</Text>
+        <UnorderedList>
+          {leagues?.map((league) => (
+            <ListItem key={league.id}>{league.name}</ListItem>
+          ))}
+        </UnorderedList>
+      </Stack>
     </main>
   );
 }
--- db/db.ts
+++ db/db.ts
@@ -5,15 +5,23 @@ export interface Team {
   name: string;
 }
 
+export interface League {
+  id?: number;
+  name: string;
+  teams: Team[];
+}
+
 export class DexieClass extends Dexie {
   // 'friends' is added by dexie when declaring the stores()
   // We just tell the typing system this is the case
   teams!: Table<Team>;
+  leagues!: Table<League>;
 
   constructor() {
     super("simple-fm-db");
-    this.version(1).stores({
+    this.version(3).stores({
       teams: "++id, name",
+      leagues: "++id",
     });
   }
 }
